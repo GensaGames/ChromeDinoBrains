35200ff2faf49adc3289e80fbf41d4da6b72678f
GensaGames <GensaGames@gmail.com>
2018-01-14 19:41:48 +0200

    Updated trained results.

--- app/ScanSetting.py
+++ app/ScanSetting.py
@@ -1,20 +1,14 @@
 
 ###################################################
-# TODO (Comments) Check this list already solved.
 # Scanner Settings! List of items with possible improvements, which
 # were skipped for now, to save performance.
-# - CACTI. Skipper several pixels for cacti top height.
-# - CACTI. Skipped Branch calculation for both sides.
-# - CACTI. Skipped stem width calculation.
-# - BIRD. Skipped calculation for the Width!
-# - BIRD. Hardcoded Bird Height, since bird always static.
 # - BIRD. Searching to the bird on Bird Head range!
 # - OTHER. Game End checking hardcoded position.
 
 
 ###################################################
 PLAYABLE_AREA_X = (50, 430)
-PLAYABLE_AREA_Y = (30, 125)
+PLAYABLE_AREA_Y = (70, 125)
 PLAYABLE_SEARCH_Y_RANGE = 10
 PLAYABLE_NEARBY_X_RANGE = 20
 
